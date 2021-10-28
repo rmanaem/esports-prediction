3fa6b3969f69f87a97dcf254b0f0aa6a6a3ea95f
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2021-10-27 21:54:51 -0400

    compile some stats

--- src/scrapers/lolesports.py
+++ src/scrapers/lolesports.py
@@ -13,17 +13,23 @@ LEAGUES = [
     'msi',
 ]
 
+HEADERS = {
+        'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
+    }
+def get_window(game_id, start_time=None):
+    url = "https://feed.lolesports.com/livestats/v1/window/%s" % game_id
+    if (start_time is not None):
+        url += "?startingTime=%s" % start_time
+    response = requests.request("GET", url, headers=HEADERS)
+    return response.json()
 def get_games(gameIds):
     url = "https://esports-api.lolesports.com/persisted/gw/getGames?hl=en-US"
     if (len(gameIds) > 0):
         url += "&id=" + ",".join(gameIds)
     payload={}
-    headers = {
-        'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
-    }
     # print(url)
     # return
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=HEADERS)
 
     return response.json()['data']['games']
 def get_completed_events(tournamentIds):
@@ -32,34 +38,28 @@ def get_completed_events(tournamentIds):
     if (len(tournamentIds) > 0):
         url += "&tournamentId=" + ",".join(tournamentIds)
     payload={}
-    headers = {
-        'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=HEADERS)
     return response.json()['data']['schedule']['events']
 
 def get_tournaments_for_league(leagues=[]):    
     url = "https://esports-api.lolesports.com/persisted/gw/getTournamentsForLeague?hl=en-US"
     if (len(leagues) > 0):
         url += "&leagueId=" + ",".join(leagues)
-
     payload={}
-    headers = {
-        'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=HEADERS)
     return [(t['slug'], t['id']) for league in response.json()['data']['leagues'] for t in league['tournaments']]
 
 def scrape():
     # ok
-    cached = os.path.isfile('lol-games.csv')
-    if cached:
+    
+    if os.path.isfile('lol-games.csv'):
         print("cached!")
         with open('lol-games.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
             next(reader, None)
             events = [tuple(row) for row in reader]
     else:
+    
         print("not cached!")
         
         # Step 0: Connect to the unofficial league of legends api
@@ -94,21 +94,90 @@ def scrape():
     
     # Step 3: For each event in a schedule, find the event details and collect all of their games and start times
     # use /getEventDetails
-    all_game_ids = [e[2] for e in events]
-    chunks = [all_game_ids[i:i+400] for i in range(0, len(all_game_ids), 400)]
-    for game_ids in chunks:
-        games = get_games(game_ids)
-        print(len(games))
-        print(games[:5])
-        break
-    reverse_offset = {}
-    for game in games:
-        reverse_offset[game['id']] = next((x['offset'] for x in game['vods'] if 'offset' in x), None)
-    
-    super_events = [(e[0], e[1], e[2], e[3], reverse_offset[e[2]]) for e in events if e[2] in reverse_offset]
-    print(super_events)
+    # if os.path.isfile('lol-games-extended.csv'):
+    #     print("Cached (Game VODs)")
+    #     with open('lol-games-extended.csv', newline='') as csvfile:
+    #         reader = csv.reader(csvfile)
+    #         next(reader, None)
+    #         game_vods = [tuple(row) for row in reader]
+    # else:
+    #     print("Not cached (Game VODs)!")
+    #     all_game_ids = [e[2] for e in events]
+    #     chunks = [all_game_ids[i:i+400] for i in range(0, len(all_game_ids), 400)]
+    #     games = []
+    #     for game_ids in chunks:
+    #         print("chunking")
+    #         games.extend(get_games(game_ids))
+
+    #     reverse_offset = {}
+    #     for game in games:
+    #         reverse_offset[game['id']] = next((x['offset'] for x in game['vods'] if 'offset' in x), None)
+        
+    #     game_vods = [(e[0], e[1], e[2], e[3], reverse_offset[e[2]]) for e in events if e[2] in reverse_offset]
+
+    #     with open('lol-games-extended.csv', 'w', newline='') as out:
+    #         csv_out=csv.writer(out)
+    #         csv_out.writerow(('tournament', 'match_id', 'game_id', 'start_time', 'offset'))
+    #         csv_out.writerows(game_vods)
     
     # Step 4: For each game, collect the live game details, and collect all relevant information on team and players
     #         At a given point of the game time
     # use /window for this
     # To get results, you must use the gameStartTime and the offset in order to add them up and find the time at which point there is information
+    for game in events:
+        window = get_window(game[2])
+        game_metadata = window['gameMetadata']
+        blue_team = window['gameMetadata']['blueTeamMetadata']
+        red_team = window['gameMetadata']['redTeamMetadata']
+        frame = window['frames'][0];
+        # break
+        # Data Extracted
+        patch_version = game_metadata['patchVersion']
+        # 1,6 - top; 2,7 - jungle; 3,8 - mid; 4,9 - bottom; 5,10 - support
+        blu_champions = [c['championId'] for c in blue_team['participantMetadata']]
+        red_champions = [c['championId'] for c in red_team['participantMetadata']]
+        
+        blu_creep = [p['creepScore'] for p in frame['blueTeam']['participants']]
+        red_creep = [p['creepScore'] for p in frame['redTeam']['participants']]
+
+        blu_kda = [(p['kills'], p['deaths'], p['assists']) for p in frame['blueTeam']['participants']]
+        red_kda = [(p['kills'], p['deaths'], p['assists']) for p in frame['redTeam']['participants']]
+
+        blu_state = [
+            frame['blueTeam']['totalGold'],
+            frame['blueTeam']['inhibitors'],
+            frame['blueTeam']['towers'],
+            frame['blueTeam']['barons'],
+            frame['blueTeam']['totalKills'],
+            len(frame['blueTeam']['dragons']),
+        ]
+        red_state = [
+            frame['redTeam']['totalGold'],
+            frame['redTeam']['inhibitors'],
+            frame['redTeam']['towers'],
+            frame['redTeam']['barons'],
+            frame['redTeam']['totalKills'],
+            len(frame['redTeam']['dragons']),
+        ]
+        print('================================')
+        print(patch_version)
+        print('================================')
+        print(blu_champions)
+        print(red_champions)
+        print('================================')
+        print(blu_state)
+        print('================================')
+        print(red_state)
+        print('================================')
+        print(blu_creep)
+        print(red_creep)
+        print('================================')
+        print(blu_kda)
+        print(red_kda)
+    # print(len(game_vods))
+    # print([g for g in game_vods if g[4] != ''])
+    # print(len([g for g in game_vods if g[4] != '']))
+    # for game in game_vods:
+    #     print(game)
+        
+        break
